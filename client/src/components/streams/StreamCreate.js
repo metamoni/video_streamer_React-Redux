e227c03f54790949241b937fb4f3dc0058cba102
metamoni <monica.mateiu@gmail.com>
2019-09-12 15:38:45 +0100

    add redux form and pass values with handleSubmit

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    // return <input {...formProps.input} />
+    return (
+      <div className="field">
+        <label htmlFor={input.name}>{label}</label>
+        <input id={input.name} {...input} />
+      </div>
+    )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues)
+  }
+
+  render() {
+    return(
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form">
+        <Field 
+          label="Enter Title"
+          name="title"
+          component={this.renderInput} />
+        <Field 
+          label="Enter Description"
+          name="description"
+          component={this.renderInput} />
+          <button className="ui button primary">Submit</button>
+      </form>
+    )
+  }
 };
 
-export default StreamCreate;
+export default reduxForm({ 
+  form: 'streamCreate'
+ })(StreamCreate);
