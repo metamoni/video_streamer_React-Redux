1f174890b64e62bd1cf91ed919cdf1a345ffbfe1
metamoni <monica.mateiu@gmail.com>
2019-10-13 20:56:55 +0100

    refactor Modal to make it reusable

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
-const Modal = props => {
+const Modal = ({ title, content, actions, onClose }) => {
   return ReactDOM.createPortal(
-    <main onClick={() => history.push('/')} className="ui dimmer modals visible active">
+    <main onClick={onClose} className="ui dimmer modals visible active">
       <section onClick={e => e.stopPropagation()} className="ui standard modal visible active">
-        <h1 className="header">Delete stream</h1>
-        <p className="content">Are you sure you want to delete this stream?</p>
+        <h1 className="header">{title}</h1>
+        <p className="content">{content}</p>
         <div className="actions">
-          <button className="ui button">Cancel</button>
-          <button className="ui primary button">Delete</button>
+          {actions}
         </div>
       </section>
     </main>, 
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button">Cancel</button>
+      <button className="ui button primary">Delete</button>
+    </>
+  );
+
+  const onClose = () => history.push('/');
+
   return (
-    <div>
-      Delete stream?
-      <Modal />
-    </div>
+    <>
+      <Modal
+        title="Delete stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onClose={onClose} />
+    </>
   )
 };
 
